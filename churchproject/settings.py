fbd2d435aff3884f021f62b60fe2493680fc72ae
Rahul Surendran <rahulsurendran154@gmail.com>
2025-05-05 20:26:17 +0530

    Fix: Add Render URL to ALLOWED_HOSTS

--- churchproject/settings.py
+++ churchproject/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-l)!t$w=&ny_rb1-=x0y^bly-q0ivwudouv=dv6nis!8g5p_rb(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['setlist-app-t1kx.onrender.com', 'www.setlist-app-t1kx.onrender.com']
+ALLOWED_HOSTS = ['localhost']
+
+# Add Render.com domain during deployment
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
